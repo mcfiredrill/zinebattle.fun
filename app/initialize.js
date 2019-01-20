3d38f022d3b339089e8389071f116f0f8789fc3d
Tony Miller <mcfiredrill@gmail.com>
2019-01-19 21:14:42 -0500

    add theme chooser

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
+  document.getElementById("themeButton").addEventListener('click', () =>{
+    console.log('do it');
+    var result = chooseTheme();
+    document.getElementById('result').innerHTML=result;
+  });
+
 });
+
+function chooseTheme(){
+  var nouns = [
+    "sonic the hedgehog ",
+    "dracula ",
+    "pizza ",
+    "power rangers "
+  ];
+  var verbs = [
+    " runs to ",
+    " eats at ",
+    " falls in love at ",
+    " goes to "
+  ];
+  var places = [
+    " walmart ",
+    " halloween store ",
+    " hell ",
+    " hamburger restaurant ",
+    " the zoo ",
+    " the aquarium "
+  ];
+  var modifiers = [
+    " in space",
+    " with skateboards",
+    " while brushing their teeth",
+    " on sale",
+    " in winter"
+  ];
+  var noun = nouns[Math.floor(Math.random() * nouns.length)];
+  var verb = verbs[Math.floor(Math.random() * verbs.length)];
+  var place = places[Math.floor(Math.random() * places.length)];
+  var modifier = modifiers[Math.floor(Math.random() * places.length)];
+  return noun + verb + place + modifier;
+
+}
